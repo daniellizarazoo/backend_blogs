d8fdb484b359b7521edfb2a83b26171bc7e825a1
daniellizarazoo <daniellizarazo015@gmail.com>
2024-06-11 18:25:19 -0500

    deleted userextractor middleware just for trys

--- controllers/login.js
+++ controllers/login.js
@@ -23,7 +23,7 @@ loginRouter.post('/',async(req,res)=>{
         id : user._id
     }
 
-    const token = jwt.sign(userForToken,process.env.SECRET)
+    const token = jwt.sign(userForToken,process.env.SECRET,{expiresIn:"120s"})
 
     res
         .status(200)
